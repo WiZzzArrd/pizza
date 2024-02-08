6dc03a259c09fe78c8d7346d3537f4e85d8868c1
eduard <eduardbordukov3@gmail.com>
2024-02-08 21:34:18 +0500

    added components, scss files

--- pizza-project/src/App.tsx
+++ pizza-project/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./scss/app.scss";
+import Header from "./components/Header/Header.jsx";
+import Sort from "./components/Sort/Sort.jsx";
+import Categories from "./components/Categories/Categories.jsx";
+import Catalog from "./components/Catalog/Catalog.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='wrapper'>
+      <Header></Header>
+
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories></Categories>
+            <Sort></Sort>
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <Catalog></Catalog>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
---
+++ pizza-project/src/components/Catalog/Catalog.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Pizza from "./Pizza/Pizza";
+
+export default function Catalog() {
+  return (
+    <div className='content__items'>
+      <Pizza></Pizza>
+      <Pizza></Pizza>
+    </div>
+  );
+}
